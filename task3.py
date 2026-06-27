56c7ac0dc6f4193cce58dd15402445ac7d466924
Elhoseny Hassan <shebeh007@gmail.com>
2026-06-27 15:04:54 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -414,7 +414,7 @@ def init_llama_resources():
     except Exception as e:
         st.warning(f"Langfuse LlamaIndex Handler Warning: {e}")
 
-    Settings.llm = LlamaGroq(model=groq_model, api_key=Groq_api_key, temperature=0, streaming=True)
+    Settings.llm = LlamaGroq(model=groq_model, api_key=Groq_api_key, temperature=0, streaming=False)
     Settings.embed_model = HuggingFaceEmbedding(model_name=embedding_model)
 
     vector_store = SimpleVectorStore()
